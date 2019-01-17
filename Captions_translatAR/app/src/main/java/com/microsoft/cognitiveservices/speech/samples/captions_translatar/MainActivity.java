42cc20267ecefeb4a25524681f139ace85336939
Milos Bijelic <mbijelic.03@gmail.com>
2019-01-17 15:27:08 -0500

    all bottom buttons light up individually

--- Captions_translatAR/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/captions_translatar/MainActivity.java
+++ Captions_translatAR/app/src/main/java/com/microsoft/cognitiveservices/speech/samples/captions_translatar/MainActivity.java
@@ -2,11 +2,15 @@ package com.microsoft.cognitiveservices.speech.samples.captions_translatar;
 
 import android.content.Intent;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -39,24 +43,24 @@ public class MainActivity extends AppCompatActivity {
         //changing project title
         setTitle("InterpretAR");
 
-        //defining the array in spinner languageIN
-        Spinner spinner = (Spinner) findViewById(R.id.LanguageIN);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.LanguageIN, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
-
-        //defining the second array in spinner LanguageOUT
-        Spinner spinnerout = (Spinner) findViewById(R.id.ClosedCaptions);
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapterout = ArrayAdapter.createFromResource(this,R.array.LanguageOUT, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears
-        adapterout.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner
-        spinnerout.setAdapter(adapterout);
+//        //defining the array in spinner languageIN
+//        Spinner spinner = (Spinner) findViewById(R.id.LanguageIN);
+//        // Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.LanguageIN, android.R.layout.simple_spinner_item);
+//        // Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // Apply the adapter to the spinner
+//        spinner.setAdapter(adapter);
+//
+//
+//        //defining the second array in spinner LanguageOUT
+//        Spinner spinnerout = (Spinner) findViewById(R.id.ClosedCaptions);
+//        // Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter<CharSequence> adapterout = ArrayAdapter.createFromResource(this,R.array.LanguageOUT, android.R.layout.simple_spinner_item);
+//        // Specify the layout to use when the list of choices appears
+//        adapterout.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // Apply the adapter to the spinner
+//        spinnerout.setAdapter(adapterout);
 
     }
 
@@ -69,37 +73,114 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
+    // controls the CC button
+    boolean CCisOn = true;
+    public void CCbutonClicked(View view) {
+        ImageView button = (ImageView) view;
+        int CCicon;
 
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radio_pirates:
-                if (checked)
-                    stringResultASL ="ON";
-                    break;
-            case R.id.radio_ninjas:
-                if (checked)
-                    stringResultASL ="OFF";
-                    break;
+        if (CCisOn) {
+            CCisOn = false;
+            CCicon = R.drawable.cc_on;
+        } else {
+            CCisOn = true;
+            CCicon = R.drawable.cc_off;
         }
+        button.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), CCicon));
+
     }
 
-    public void onRadioButtonClickedCC(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
+    // controls the ASL button
+    boolean ASLisOn = true;
+    public void ASLbuttonClicked(View view) {
+        ImageView button = (ImageView) view;
+        int ASLicon;
 
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radio_piratesCC:
-                if (checked)
-                    stringResultCC ="ON";
-                    break;
-            case R.id.radio_ninjasCC:
-                if (checked)
-                    stringResultCC ="OFF";
-                    break;
+
+        if (ASLisOn) {
+            ASLisOn = false;
+            ASLicon = R.drawable.asl_on;
+
+        } else {
+            ASLisOn = true;
+            ASLicon = R.drawable.asl_off;
         }
+        button.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), ASLicon));
     }
+
+    // controls the input language button
+    boolean inputLangOn = true;
+    public void InputLangSwitch(View view) {
+        ImageView button = (ImageView) view;
+        int inputLangIcon;
+
+
+        if (inputLangOn) {
+            inputLangOn = false;
+            inputLangIcon = R.drawable.input_lang_on;
+
+        } else {
+            inputLangOn = true;
+            inputLangIcon = R.drawable.input_lang_off;
+        }
+        button.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), inputLangIcon));
+    }
+
+    // controls the ASL button
+    boolean outputLangOn = true;
+    public void OutputLangSwitch(View view) {
+        ImageView button = (ImageView) view;
+        int outputLangIcon;
+
+
+        if (outputLangOn) {
+            outputLangOn = false;
+            outputLangIcon = R.drawable.output_lang_on;
+
+        } else {
+            outputLangOn = true;
+            outputLangIcon = R.drawable.output_lang_off;
+        }
+        button.setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), outputLangIcon));
+    }
+
+
+//    public void onRadioButtonClicked(View view) {
+//        // Is the button now checked?
+//        boolean checked = ((RadioButton) view).isChecked();
+//
+//        // Check which radio button was clicked
+//        switch(view.getId()) {
+//            case R.id.radio_pirates:
+//                if (checked)
+//                    stringResultASL ="ON";
+//                    break;
+//            case R.id.radio_ninjas:
+//                if (checked)
+//                    stringResultASL ="OFF";
+//                    break;
+//        }
+//    }
+
+//    public void onRadioButtonClickedCC(View view) {
+//        // Is the button now checked?
+//        boolean checked = ((RadioButton) view).isChecked();
+//
+//        // Check which radio button was clicked
+//        switch(view.getId()) {
+//            case R.id.radio_piratesCC:
+//                if (checked)
+//                    stringResultCC ="ON";
+//                    break;
+//            case R.id.radio_ninjasCC:
+//                if (checked)
+//                    stringResultCC ="OFF";
+//                    break;
+//        }
+//    }
+
+//    public void onLogoClicked(View view) {
+//        Intent intent = new Intent(this, TranslatarActivity.class);
+//        startActivity(intent);
+//    }
 }
