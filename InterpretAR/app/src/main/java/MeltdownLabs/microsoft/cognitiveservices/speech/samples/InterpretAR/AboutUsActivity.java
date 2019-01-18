05a9ebfe99d8c35be6e298caf50b026f205041b0
Milos Bijelic <mbijelic.03@gmail.com>
2019-01-17 21:55:29 -0500

    changed project name to InterpretAR, added new app icon

---
+++ InterpretAR/app/src/main/java/MeltdownLabs/microsoft/cognitiveservices/speech/samples/InterpretAR/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package MeltdownLabs.microsoft.cognitiveservices.speech.samples.InterpretAR;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.microsoft.cognitiveservices.speech.samples.InterpretAR.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
