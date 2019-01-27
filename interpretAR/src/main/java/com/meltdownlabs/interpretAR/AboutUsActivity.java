0a81e1694bff5ea12d643582771b5154ca57ad07
Brandon Rufino <brandonrufino@hotmail.cagit config --global user.emailgit config --global user.email brandonrufino@hotmail.ca>
2019-01-26 19:22:58 -0500

    ASL option leads to unity engine. CC fully functioning. updating drawable. working on unity engine for ASL integration.

---
+++ interpretAR/src/main/java/com/meltdownlabs/interpretAR/AboutUsActivity.java
@@ -0,0 +1,15 @@
+package com.meltdownlabs.interpretAR;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.meltdownlabs.interpretAR.R;
+
+public class AboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
