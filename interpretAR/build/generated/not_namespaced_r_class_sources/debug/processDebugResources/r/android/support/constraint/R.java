303331148829974067a002ac988498579604f777
Milos Bijelic <mbijelic.03@gmail.com>
2019-03-12 17:27:29 -0400

    added scrolling on about us page

--- interpretAR/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ interpretAR/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f070021;
-        public static final int end = 0x7f070036;
-        public static final int gone = 0x7f070040;
-        public static final int invisible = 0x7f07004d;
-        public static final int left = 0x7f070051;
-        public static final int packed = 0x7f07005f;
-        public static final int parent = 0x7f070060;
-        public static final int percent = 0x7f070062;
-        public static final int right = 0x7f070067;
-        public static final int spread = 0x7f07007f;
-        public static final int spread_inside = 0x7f070080;
-        public static final int start = 0x7f070085;
-        public static final int top = 0x7f07009a;
-        public static final int wrap = 0x7f0700a1;
+        public static final int end = 0x7f070038;
+        public static final int gone = 0x7f070042;
+        public static final int invisible = 0x7f07004f;
+        public static final int left = 0x7f070053;
+        public static final int packed = 0x7f070061;
+        public static final int parent = 0x7f070062;
+        public static final int percent = 0x7f070064;
+        public static final int right = 0x7f070069;
+        public static final int spread = 0x7f070081;
+        public static final int spread_inside = 0x7f070082;
+        public static final int start = 0x7f070087;
+        public static final int top = 0x7f07009c;
+        public static final int wrap = 0x7f0700a3;
     }
     public static final class styleable {
         private styleable() {}
